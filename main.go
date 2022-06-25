63e506e0db0e8579c05da9696fc1ccdf4f432332
khanjasir90 <khanmohdjasir@gmail.com>
2022-06-25 09:36:57 +0530

    fix minor changes related to ci pipeline

--- main.go
+++ main.go
@@ -14,23 +14,26 @@ func goDotEnvVariable(key string) string {
 	err := viper.ReadInConfig()
 	if err != nil {
 		fmt.Println("Error while reading Env File")
+		return "3000"
 	}
 	value, ok := viper.Get(key).(string)
 	if !ok {
 		fmt.Println("Error while getting key related to key")
+		return "3000"
 	}
 	return value
 }
 
 func testRoute(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	fmt.Println("On test route")
-	json.NewEncoder(w).Encode("On test route")
+	json.NewEncoder(w).Encode("on test route")
 }
 
 func main() {
 	PORT := goDotEnvVariable("PORT")
 	fmt.Println(PORT)
 	router := mux.NewRouter()
-	router.HandleFunc("/test", testRoute)
+	router.HandleFunc("/test", testRoute).Methods("GET")
 	log.Fatal(http.ListenAndServe(PORT,router))
 }
